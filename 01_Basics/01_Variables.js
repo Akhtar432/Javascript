114b05b9690d7023dc55d142d76c5cf9cdca0379
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-07-02 10:38:19 +0000

    Variables done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = "14425"
+let accountEmail = "intezarakhtar@gmail.com"
+var accountPassward = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 Not Allowed Bcz accountId is const
+
+let accountAdress;
+
+//modify
+accountEmail = "akh@gmail"
+accountPassward = "213213"
+accountCity = "Patna"
+
+console.table([accountId, accountEmail, accountPassward, accountAdress])
