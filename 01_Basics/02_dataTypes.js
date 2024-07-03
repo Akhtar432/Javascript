6fdfef427e85b1acffe14d89b13193aa31b21584
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-07-03 07:23:39 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"  // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log( 3
+     +
+     3) // code readablity shoud be high
+
+console.log("Akhtar")
+
+let name = "Akhtar"
+let age  = 18
+let isLogged =  false
+
+// number ==> 2 to power 53
+// bigint
+// boolean = true / false
+// null ===> standalone value
+// undefinded
+// symbol ==> unique
+
+// object
+
+console.log(typeof "Akhtar")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
