bf52a423b0220f0b05172ae5a069b0cebe474fd4
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-07-04 16:17:34 +0000

    Conversion and operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score)
+// console.log(typeof score)
 // or console.log(typeof (score)) same
 
 let valueInNumber = Number(score) // conversion
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 // "33" ==> 33
 // "33abc" ==> Nan
@@ -13,7 +13,7 @@ console.log(typeof valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // " " => false
@@ -21,5 +21,27 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Operations ****************************
+
+let value = 3;
+let negValue = -value
+console.log(negValue)
+
+// Arithmatic all perform
+
+let str1 = "Hello"
+let str2 = "Akhtar"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(+"") // 0
