1cf146ae8b1df04e59bc11ae10fcec3e93a6072c
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-07-05 06:31:21 +0000

    Datatypes-Summaries

---
+++ 01_Basics/dataTypes-Summeries.js
@@ -0,0 +1,31 @@
+// Primitive datatype
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id === anotherId) // false
+
+const bigNumber = 23658123762367893n
+
+
+// Non-Primitive ( Referance )
+
+// Array, objects, Function
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "Akhtar",
+    age: 22
+}
+
+const myFunction = function(){
+    console.log("Hello Duniya");
+}
