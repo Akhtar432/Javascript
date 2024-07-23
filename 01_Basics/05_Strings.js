8d7d88575eaca2598619bd103efd2d71101a278d
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-07-23 12:46:37 +0000

    Strings

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,25 @@
+const name = "Akhtar"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log('Hello My Name is ${name} and my repoCount is ${repoCount}');
+
+const gameName = new String ('hiteshtcs')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt('4'));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+console.log(anotherString);
+
+const newStringone = "   Akhtar   ";
+console.log(newStringone);
+console.log(newStringone.trim());
