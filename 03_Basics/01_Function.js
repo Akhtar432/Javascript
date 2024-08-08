eac51bf14a022691a5dbc67eb0f047f6ff6c9d56
Intezar Akhtar <117929980+Akhtar432@users.noreply.github.com>
2024-08-08 17:12:27 +0000

    Funtions

--- 03_Basics/01_Function.js
+++ 03_Basics/01_Function.js
@@ -32,4 +32,34 @@ function logingUserMessage(username){
 }
 
 // console.log(logingUserMessage("Akhtar"))
-console.log(logingUserMessage())
+// console.log(logingUserMessage())
+
+function calculateCartPrize(...num1){
+    return num1
+}
+
+console.log(calculateCartPrize(200, 300, 400, 500))
+
+const user = {
+    username: "Akhtar",
+    prize: 199
+}
+
+function handleObject(anyobject){
+    console.log('Username is ${anyobject.username} and prize is ${anyobject.prize}');
+}
+
+// handleObject(user)\
+
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 100, 600]))
